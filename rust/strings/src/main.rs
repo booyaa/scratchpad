4f246c080b1854b17be7be72cdb31d73b73723c5
booyaa <booyaabooyaabooyaa@gmail.com>
2016-07-08 08:44:18 +0100

    hello world!

---
+++ rust/strings/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let foo = "foo";
+    let bar = "bar".to_string(); // String::from("bar") is okay too
+
+    // as_string(foo); // won't work because it's a referenced string slice &str
+    as_string(foo.to_string()); // but this will work because we've coerced it into a String
+    as_string(bar); // okay, because it's a String
+
+    as_string_slice(foo); // okay
+    // as_string_slice(bar); // won't work because we're expecting a referenced string slice
+}
+
+fn as_string(thing: String) {
+    println!("as_string: {}", thing);
+}
+
+fn as_string_slice(thing: &str) {
+    println!("as_string_slice: {}", thing);
+}
+
+fn concatenate_string(this: String, that: String) {}
+
+fn concatenate_str(this: &str, that: &str) {}
