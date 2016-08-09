630a225908a2d07be1a70b37e3b362e47d0e3206
booyaa <booyaabooyaabooyaa@gmail.com>
2016-08-09 07:45:46 +0100

    understanding how pathbuf works

--- rust/read_file/src/main.rs
+++ rust/read_file/src/main.rs
@@ -1,10 +1,20 @@
 use std::fs::File;
 use std::io::Read;
 
-
-fn
 fn main() {
-    let mut file = File::open("Cargo.toml").unwrap();
+    // let mut file = File::open("Cargo.toml").unwrap();
+
+    use std::path::Path;
+    assert_eq!(Path::new("/Users/booyaa/Dev/rust/foundry/wifiscanner/tests/fixtures/airport/airport01.txt")
+                   .is_file(),
+               true);
+
+    let mut file =
+        File::open("/Users/booyaa/Dev/rust/foundry/wifiscanner/tests/fixtures/airport/airport01.\
+                    txt")
+            .unwrap();
+
+
 
     let mut contents: Vec<u8> = Vec::new();
     // Returns amount of bytes read and append the result to the buffer
@@ -12,6 +22,16 @@ fn main() {
     println!("Read {} bytes", result);
 
     // To print the contents of the file
+
     let filestr = String::from_utf8(contents).unwrap();
-    println!("{}", filestr);
+    // println!("{:?}", filestr);
+
+    let mut lines = filestr.lines();
+    let first_line = lines.next().unwrap();
+
+    println!("header: {}", first_line);
+
+    for line in lines {
+        println!("other: {}", line);
+    }
 }
