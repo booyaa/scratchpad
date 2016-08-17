8adcae4167a0cba1e6d20282ad61f81af2a07315
booyaa <booyaabooyaabooyaa@gmail.com>
2016-08-17 17:42:21 +0100

    add

---
+++ rust/syllables/src/main.rs
@@ -0,0 +1,8 @@
+// https://github.com/nltk/nltk_contrib/blob/master/nltk_contrib/readability/syllables_en.py
+// https://github.com/yasnis/SyllableCounter/blob/master/SyllableCounter.js
+// https://github.com/wfreitag/syllable-counter-swift/blob/master/SyllableCounter.swift
+// https://github.com/DigTheDoug/SyllableCounter/blob/master/SyllableCounter.py
+// https://github.com/IKEARiot/SyllableCounter/blob/master/NaiveSyllableCounter/SyllableCounter.cs
+fn main() {
+    println!("Hello, world!");
+}
