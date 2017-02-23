ca0074f9122cf35d1d7522c98454ca76b880e9e6
booyaa <booyaabooyaabooyaa@gmail.com>
2017-02-23 11:25:39 +0000

    terminal escape codes

---
+++ rust/terminal/src/main.rs
@@ -0,0 +1,13 @@
+fn main() { //R O Y G B I V
+    print!("\x1B[31mU\x1B[0m");
+    print!("\x1B[32mN\x1B[0m");
+    print!("\x1B[33mI\x1B[0m");
+    print!("\x1B[34mC\x1B[0m");
+    print!("\x1B[35mO\x1B[0m");
+    print!("\x1B[36mR\x1B[0m");
+    print!("\x1B[37mN\x1B[0m");
+    println!("\x1B[38m!\x1B[0m");
+
+    println!("\u{2}\u{3}0,4 IT'S DANGEROUS!! \u{f} right?")
+
+}
